cdd10f196ccc1b9f6f12b39ff13096f07b65c39e
“qqlcx5” <qqlcx5@gmail.com>
2020-05-05 18:03:39 +0800

    init

---
+++ src/views/layout/components/NavMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-menu
+    default-active="2"
+    :collapse="isCollapse"
+    :background-color="variables.menuBg"
+    :text-color="variables.menuText"
+    :unique-opened="false"
+    :active-text-color="variables.menuActiveText"
+    :collapse-transition="false"
+    mode="vertical"
+  >
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span slot="title">导航一</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span slot="title">导航二</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <i class="el-icon-document"></i>
+      <span slot="title">导航三</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <span slot="title">导航四</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+<script>
+import variables from "@/assets/css/variables.scss";
+export default {
+  data() {
+    return {
+      isCollapse: false
+    };
+  },
+  computed: {
+    variables() {
+      return variables;
+    }
+  }
+};
+</script>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="layout">
+    <div class="sidebar">
+      <NavMenu />
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+import NavMenu from "./components/NavMenu";
+export default {
+  components: {
+    NavMenu
+  }
+};
+</script>
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  min-height: 100vh;
+  max-height: 100%;
+  .sidebar {
+    width: 210px;
+    overflow: hidden;
+    background: $menuBg;
+  }
+  .main {
+    flex: 1;
+  }
+}
+</style>
